bf0c1d98a23d6bb14daf3009179f6b54c443ece1
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-06-19 15:12:15 +0530

    Revert "Better Floodwait handling"
    This reverts commit e3b802c7e50d754bdfcabfb43853b4fb66ecb671.

--- plugins/auto_reply.py
+++ plugins/auto_reply.py
@@ -8,22 +8,15 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 lock = asyncio.Lock()
-replied = 0
 
 @Client.on_message(filters.chat(FROM_GRP))
 async def auto_reply(bot, message):
-    global replied
     async with lock:
         try:
             await message.reply_text('''Meri Profile Pr Jaao Wahan Movie Search Group Hai Wahan Movie Search Kro Free me No Ads Join And Enjoy..
 
 @Arpitbotmovies
 @Arpitbotmovies''', reply_to_message_id=message.id)
-            replied += 1
-            await asyncio.sleep(2)
-            if replied % 20 == 0:
-                logger.info("⏸️ Replied to 20 messages! Taking a break of 30 seconds...")
-                await asyncio.sleep(30)
         except FloodWait as e:
             logger.warning(f"Got FloodWait.\n\nWaiting for {e.value} seconds.")
             await asyncio.sleep(e.value + 2)
