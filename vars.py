e66d84c4ad715c1a9e575d7ab61fdcc8c4b8b1b6
MovieBots <154045205+MovieBots@users.noreply.github.com>
2025-06-27 12:56:14 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "N2 Auto Reply Bot")
 API_ID = 27732327
 API_HASH = "7c7c0a2b3b5dc1eb6b84433228dcf4c8"
-USER_SESSION = environ.get("USER_SESSION", "BQGnKWcAlqPO45_eOHMD38mcvBXSHgppo0Kg8AChKgD40YJQHX7vVyrPffIiBbgcTJ8X60yiBDjJnUBaVcKI_rDZwjsbJ873wAWI63L8Z-s8LgGsPskeDO5GIqUrDQbm3RgGy4ecldViaxPaIm46h31I5GT9oX9A7M3N9MZgIUfbqo70pu0R7APjFNs35xiHnDBGkmA2yhE54eEOpZR5FJu8nXBHSV59U6k0eWqbxjzk3wb5qeN1gWwX1SvalaM7QcKePJRYdhP4c3vtR4CWm32RC1ModPsA1wxRP80Uy-6hygYRoSkiCiGoHIhDMKbevqWUHXC1jvfHre_avC0_TcgnMllUiwAAAAHVJZ9QAA")
+USER_SESSION = environ.get("USER_SESSION", "BQGnKWcAJ8qon2MNSAOXZl89b8t9Tleu7SUelCyN5rX5cwdUTLv5Xnx4_bNPiX0i_g8B0k3FoBVt0tleUo3SXJF0JpAz8Rws3vXjRh5UciiLd80kxGPuCn5EDoJ5FFk_zpUSfo0R-RINuTFYGZp2L3VYxv50xRRfue-SsFkmwwcm4epjYUrddXOQphU8STbpl0c0D3Kf3L7mMMyVfq14OD0od4HCTsJoTuNUfKMb7k7yPfsh4uNnde6dIYNnePRB2ff89HKqvd3I4qtNwRWxlAjFFJ6onMlnjqS801ulekDS7qXX6xiwXlruBobPsAtfuYND69vsZqrC0jcUaKHQxIrxB9LArQAAAAHVJZ9QAA")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002074744533"))
 FROM_GRP = [int(channel_id) for channel_id in environ.get('FROM_GRP', '-1002216169351 -1002162221322').split() if re.match(r'^-?\d+$', channel_id)]
 PORT = int(environ.get("PORT", "8080"))
