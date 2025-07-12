2c7a7f6216ab502e67bdda0c096e21cbf3dc12d5
MovieBots <154045205+MovieBots@users.noreply.github.com>
2025-07-12 13:50:09 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "N2 Auto Reply Bot")
 API_ID = 28813019
 API_HASH = "cc02b3685457d3109515657346ed5cfd"
-USER_SESSION = environ.get("USER_SESSION", "AQG3ptsAVSzj1mhUoer0hCY3lwK7xFRC2TCxmHXtvTKMCh7dbotRvVbWoKml6dUyC8KwwmiCujvNlWfGHKLhFg8iWpoiZShKgIoHJwu__9jDmHKp_JYX4CyDG3aslanYLNLdX9iG1kLwm5p0uJJZXurxpyj8fSTesZgegsQpg8zFSMlXEwZ3cDcMx5frGIgi1dY4Q-K-gzziBcI9xAPJwojUNySmN6gHkQEaDhm_VDyyUGgAP9EWRZBUZw43TTvbUoe8idlO2m36-WryIfVtYqkO3ybkUfDFeVYqGhxVDEyVPmJ71KQ8Nox2XJz6vKXy1xQCAM-1P0u7_68zJiBBUIY6LUflLAAAAAHXYvYoAA")
+USER_SESSION = environ.get("USER_SESSION", "")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002074744533"))
 FROM_GRP = [int(channel_id) for channel_id in environ.get('FROM_GRP', '-1002216169351 -1002162221322 -1001619702173').split() if re.match(r'^-?\d+$', channel_id)]
 PORT = int(environ.get("PORT", "8080"))
