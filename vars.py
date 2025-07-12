bee914e69e92e9789460335809dc7dfc01aac3a5
MovieBots <154045205+MovieBots@users.noreply.github.com>
2025-07-12 13:35:43 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -11,11 +11,11 @@ def is_enabled(value, default):
         return default
 
 SESSION = environ.get("SESSION", "N2 Auto Reply Bot")
-API_ID = 29059899
-API_HASH = "bd85b34cbd5a5fd10f34b8df1b11f89a"
+API_ID = 28813019
+API_HASH = "cc02b3685457d3109515657346ed5cfd"
 USER_SESSION = environ.get("USER_SESSION", "")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002074744533"))
 FROM_GRP = [int(channel_id) for channel_id in environ.get('FROM_GRP', '-1002216169351 -1002162221322 -1001619702173').split() if re.match(r'^-?\d+$', channel_id)]
 PORT = int(environ.get("PORT", "8080"))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8064024093').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7908554280').split()]
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/MovieBots/Auto-Reply-Bot")
