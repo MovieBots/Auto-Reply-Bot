b8ac538a4bc7ecb0eceba597bc1202ad3daa722d
MovieBots <154045205+MovieBots@users.noreply.github.com>
2025-08-11 15:54:51 +0530

    Update auto_reply.py

--- plugins/auto_reply.py
+++ plugins/auto_reply.py
@@ -2,9 +2,6 @@ import asyncio, re
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from vars import FROM_GRP
-import asyncio
-DELETE_TIME = 30  # time in seconds before delete
-
 
 import logging
 logger = logging.getLogger(__name__)
@@ -16,14 +13,10 @@ lock = asyncio.Lock()
 async def auto_reply(bot, message):
     async with lock:
         try:
-            m = await message.reply_text('''𝐘𝐎𝐔𝐑 𝐌𝐎𝐕𝐈𝐄 𝐋𝐈𝐍𝐊 𝐈𝐍 𝐌𝐘 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 𝐁𝐈𝐎 𝐂𝐇𝐄𝐂𝐊 𝐀𝐍𝐃 𝐒𝐄𝐀𝐑𝐂𝐇
+            await message.reply_text('''𝐘𝐎𝐔𝐑 𝐌𝐎𝐕𝐈𝐄 𝐋𝐈𝐍𝐊 𝐈𝐍 𝐌𝐘 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 𝐁𝐈𝐎 𝐂𝐇𝐄𝐂𝐊 𝐀𝐍𝐃 𝐒𝐄𝐀𝐑𝐂𝐇
+
+
 ''', reply_to_message_id=message.id)
-await asyncio.sleep(DELETE_TIME)
-await m.delete()
-try:
-    await message.delete()
-except:
-    pass
 
             await asyncio.sleep(2)
         except FloodWait as e:
